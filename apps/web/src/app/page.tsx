250cc2e271667b358eb901781d32b241dcf89070
Thomas <the.beel.616@gmail.com>
2024-03-10 13:41:52 +0100

    improved dev mode

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,10 +1,13 @@
-import { Button, Header } from "@acme/ui";
+import { Button, Header, Input, TextInput } from "@acme/ui";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <Header text="World" />
       <Button />
+
+      <Input type="number" />
+      <TextInput />
       <div className="bg-cyan-400 p-4">
         <p>Testing</p>
       </div>
