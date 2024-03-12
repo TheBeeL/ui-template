e16aeb97401caaab92a3c05000321aedbf97bc76
Thomas <the.beel.616@gmail.com>
2024-03-12 01:59:54 +0100

    Improved nextJs

--- apps/nextjs/tailwind.config.ts
+++ apps/nextjs/tailwind.config.ts
@@ -6,7 +6,7 @@ import UIConfig from "@acme/ui/tailwindConfig";
 export default {
   // We need to append the path to the UI package to the content array so that
   // those classes are included correctly.
-  content: UIConfig.content,
+  content: [...UIConfig.content, "./src/**/*.{js,ts,jsx,tsx,mdx}"],
   presets: [UIConfig],
   theme: {
     extend: {
